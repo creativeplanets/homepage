d1cc8a831ca3f6d6e4fb370eb4be6b83ae29d15e
mors.chang <mors.chang@gmail.com>
2023-03-10 15:24:30 +0800

    Contact info

--- src/components/Homepage/Footer.tsx
+++ src/components/Homepage/Footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
             alt="START A PROJECT WITH US !"
             className={style.contact__caption}
           ></img>
-          <div className={style.contact__mail}>xxx@iisigroup.com</div>
+          <div className={style.contact__mail}>mors.chang@iisigroup.com</div>
           <Button title="EMAIL US" corner={true} />
         </div>
         <div className={style.about}>
@@ -33,7 +33,7 @@ export default function Footer() {
               <br />
               <br /> Mon - Fri / 09:00 - 18:00 <br />
               <br />
-              xxx@iisigroup.com
+              mors.chang@iisigroup.com
             </div>
             <div className={style.about__copyright}>Copyright © 2023 IISI.</div>
           </div>
